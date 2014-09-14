7847d0194c71ee0eebc32e785e605c94d332220b
TFurman <tim.r.n.furman@gmail.com>
2014-09-13 22:43:21 -0400

    continuation of exif parsing

--- ripEXIF.py
+++ ripEXIF.py
@@ -1,6 +1,20 @@
 from PIL import Image
-img = Image.open('img.jpg')
-exif_data = img._getexif()
+from PIL.ExifTags import TAGS
+import pprint
+ 
+def get_exif_data(fname):
+    """Get embedded EXIF data from image file."""
+    ret = {}
+    try:
+        img = Image.open(fname)
+        if hasattr( img, '_getexif' ):
+            exifinfo = img._getexif()
+            if exifinfo != None:
+                for tag, value in exifinfo.items():
+                    decoded = TAGS.get(tag, tag)
+                    ret[decoded] = value
+    except IOError:
+        print 'IOERROR ' + fname
+    return ret
 
-out = open("test.txt","w")
-out.write(exif_data)
+print get_exif_data("nonsense.jpg")
